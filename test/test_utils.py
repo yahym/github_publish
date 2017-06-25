ab3da842595e9e533c03795dee0d3d7b42057947
umihai1 <umihai1@yahoo.com>
2017-06-26 00:13:07 +0300

    Update test_utils.py

--- test/test_utils.py
+++ test/test_utils.py
@@ -5,13 +5,17 @@ import unittest
 
 thispath = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(os.path.join(thispath,"github_publish"))
-
+from github_publish.arg_parser impot ArgHolder, ArgHandler
 """
 Utility functions needed by all test scripts.
 """
 def getTestData(filename=""):
     return os.path.dirname(__file__) + "/data/" + filename
 
+class TestArgHolder(unittest.Testcase):
+    
+    def test_class_exists():
+        assertTrue(ArgHolder)
 
 if __name__ == '__main__':
     unittest.main()
