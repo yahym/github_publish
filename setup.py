adeb5476336f9dcd071f70440cee154eee149c87
uidl9955 <uidl9955@contiwan.com>
2017-07-01 11:45:13 +0300

    fix setup.py pypi syntax

--- setup.py
+++ setup.py
@@ -53,20 +53,18 @@ setup(
         'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
+        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Intended Audience :: Developers',
-        'Topic :: File parsers :: Measurement parsers',
+        'Topic :: Software Development :: Version Control :: Git',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: ',
+        'License :: Public Domain',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
     ],
 
     # What does your project relate to?
